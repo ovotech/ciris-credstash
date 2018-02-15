1bfa43e4def0f2a9dfe9bdcdc3bf1ab8b8a2624d
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-02-15 16:02:25 +0000

    Update library to work with ciris 0.7

--- src/main/scala/ciris/credstash/credstash.scala
+++ src/main/scala/ciris/credstash/credstash.scala
@@ -1,23 +1,38 @@
 package ciris
 
+import ciris.api._
+
 import scala.collection.mutable.ListBuffer
 import scala.sys.process._
 
 package object credstash {
+  val CredstashKeyType: ConfigKeyType[String] =
+    ConfigKeyType("credstash key")
 
-  val keyType = ConfigKeyType[String]("Credstash key")
+  def credstashSource(region: String): ConfigSource[Id, String, String] =
+    ConfigSource(CredstashKeyType) { key =>
+      val outputBuffer =
+        ListBuffer.empty[String]
 
-  def credstash[Value: ConfigReader](region: String)(key: String): ConfigValue[Value] =
-    ConfigValue(key)(credstashSource(region), ConfigReader[Value])
+      val exitValue =
+        s"credstash get -r $region $key"
+          .run(ProcessLogger(outputBuffer append _))
+          .exitValue()
 
-  def credstashSource(region: String): ConfigSource[String] = ConfigSource(keyType){ key =>
-    val outputBuffer = ListBuffer[String]()
-    val exitValue = (s"credstash get -r $region $key" run ProcessLogger(outputBuffer append _)).exitValue
+      val result =
+        outputBuffer.mkString("\n").trim
 
-    val result = outputBuffer mkString "\n" trim()
-    exitValue match{
-      case 0 => Right(result)
-      case _ => Left(ConfigError(result))
+      exitValue match {
+        case 0 => Right(result)
+        case _ => Left(ConfigError(result))
+      }
     }
+
+  def credstash[Value](region: String)(key: String)(
+      implicit decoder: ConfigDecoder[String, Value]
+  ): ConfigEntry[Id, String, String, Value] = {
+    credstashSource(region)
+      .read(key)
+      .decodeValue[Value]
   }
 }
